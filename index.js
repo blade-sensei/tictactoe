18c2145a8195f6e84aded42c35aef4985a98e129
Juan COYLA <jcoyla123@gmail.com>
2020-02-19 10:33:27 +0100

    rebuild board

--- index.js
+++ index.js
@@ -9,6 +9,29 @@
  * [[0,0],[0,1], 0,2], [[0,0], [1,1], [2,2]], [ ]]
  */
 
+
+ function createBoard(squares, container) {
+     for (let i = 0; i < squares.length; i++) {
+         const row = squares[i];
+         const squareRowElement = document.createElement('div');
+         squareRowElement.className = 'square-row';
+         for (let squareIndex = 0; squareIndex < row.length; squareIndex++) {
+             const square = row[squareIndex];
+             const squareElement = document.createElement('div');
+             squareElement.className = 'square';
+             squareElement.id = `${i}-${squareIndex}`;
+             squareElement.textContent = square;
+             squareRowElement.appendChild(squareElement);
+         }
+         container.appendChild(squareRowElement);
+     }
+     document.body.appendChild(container);
+ }
+
+
+
+
+
  const winnerCombinations = [
      [ [0,0], [0,1], [0,2] ],
      [ [1,0], [1,1], [1,2] ],
@@ -27,10 +50,12 @@
 
 
  const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
+    ['X', '', ''],
+    ['', 'X', ''],
+    ['', '', 'O'],
 ]
+
+createBoard(board, document.querySelector('.square-container'));
 
  const game = {
     currentPlayerTurn: 'player1',
@@ -41,7 +66,6 @@
          'draw': 0,
      },
      set board(value) {
-        console.log('ok');
         this._board = value;
     },
     get board() {
@@ -75,11 +99,24 @@ function createSquaresInEachRow(rows) {
     }
 }
 
+function updateBoard ([row, column], game) {
+    const updatedBoard = game.board;
+    const playerMark = getPlayerMark(game.currentPlayerTurn);
+    updatedBoard[row][column] = playerMark;
+
+    return updatedBoard;
+
+}
+
 function createSquareInRow(row, rowNumber) {
     for (let i = 0; i < 3; i++) {
-        row.appendChild(createSquare(rowNumber, i));
+        row.appendChild(createSquare(rowNumber, i, game));
         document.body.appendChild(row);
     }
+}
+
+function getPlayerMark(playerName) {
+    return playerName === 'player1' ? 'X': 'O';
 }
 
 const squares = Array.from(document.querySelectorAll('.square'));
@@ -89,8 +126,10 @@ setSquareEventListener(squares);
 function setSquareEventListener(squares) {
     squares.forEach(square => {
         square.addEventListener('click', (event) => {
+            const square = event.target;
+            const squareCoordinates = getSquareCoordinates(square);
+            game.board = updateBoard(squareCoordinates, game);
             game.currentPlayerTurn = getNextPlayerTurn(game.currentPlayerTurn);
-            renderMarkPlayer(square)
             if (checkWinner()) {
                 alert(`winner ${previous} player`);
             }
@@ -98,8 +137,10 @@ function setSquareEventListener(squares) {
     })
 }
 
-function renderMarkPlayer(element) {
-    element.textContent = game.currentPlayerTurn;
+
+function getSquareCoordinates(square) {
+    const coordinates = square.id.split('-');
+    return coordinates.map(cordinate => Number(cordinate));
 }
 
 function getNextPlayerTurn(currentPlayerTurn) {
@@ -108,11 +149,16 @@ function getNextPlayerTurn(currentPlayerTurn) {
     
 }
 
-function createSquare(rowNumber, columnNumber) {
+function addMarkToTable(game, square) {
+    const board = JSON.parse(JSON.stringify(game.board));
+}
+
+function createSquare(rowNumber, columnNumber, game) {
     const column = document.createElement('div');
     column.className = 'square';
     column.id = `${rowNumber}-${columnNumber}`;
-    column.textContent = ' - ';
+    const data = game.board[rowNumber][columnNumber];
+    column.textContent = data;
     return column;
 
 }
